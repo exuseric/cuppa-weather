74b666b19c52a1b833cedc376ef4acf1b131e07d
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-08-13 10:26:02 +0300

    update homepage

---
+++ src/components/homepage/page-header.svelte
@@ -0,0 +1,35 @@
+<script>
+  import SearchBar from '$components/search/search-bar.svelte';
+</script>
+
+<header class="header">
+  <div class="container">
+    <div class="text-group">
+      <div class="heading text-wrapper">
+        <h1>A cuppa that's right for your weather.</h1>
+      </div>
+      <SearchBar />
+    </div>
+  </div>
+</header>
+
+<style lang="scss">
+  @use 'sass:math';
+
+  .header {
+    @include center;
+    margin-top: $nav-height;
+    min-height: calc(100vh - $nav-height);
+  }
+
+  .heading {
+    margin-bottom: $spacing-lg;
+
+    h1 {
+      font-family: $heading-family;
+      @include font(h3);
+
+      color: $primary-500;
+    }
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import AboutSection from '$components/homepage/about-section.svelte';
+  import LinksSection from '$components/homepage/links-section.svelte';
+  import Header from '$components/homepage/page-header.svelte';
+</script>
+
+<svelte:head>
+  <title>Cuppa | Tea and Weather</title>
+  <meta name="keywords" content="cuppa homepage" />
+</svelte:head>
+
+<Header />
+<main>
+  <AboutSection />
+</main>
