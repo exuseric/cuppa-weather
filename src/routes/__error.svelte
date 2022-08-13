b6b1c2a10b24b8a7d30775bddf5ed10e92e4bd2e
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-08-13 10:26:33 +0300

    update layouts

---
+++ src/routes/__error.svelte
@@ -0,0 +1,75 @@
+<script context="module">
+  /** @type {import('@sveltejs/kit').Load} */
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}`
+      }
+    };
+  }
+</script>
+
+<script>
+  import Sun from '$components/icons/sun.svelte';
+
+  export let title;
+</script>
+
+<svelte:head>
+  <title>{title} | Cuppa</title>
+</svelte:head>
+
+<section class="error-page section">
+  <header class="container ">
+    <div class="text-group">
+      <h1>{title}</h1>
+      <p>That wasn't supposed to happen.</p>
+      <a href="/">Let's go back home.</a>
+    </div>
+    <div class="sun">
+      <Sun />
+    </div>
+  </header>
+</section>
+
+<style lang="scss">
+  .error-page {
+    @include center;
+
+    height: calc(100vh - $nav-height);
+  }
+  header {
+    @include flex-wrap-row;
+    align-items: center;
+    justify-content: space-between;
+
+    position: relative;
+    isolation: isolate;
+    overflow: hidden;
+
+    height: 100%;
+
+    // border-radius: 50%;
+
+    h1 {
+      position: relative;
+      z-index: 2;
+
+      // font-size: size(h1);
+      @include font(h1);
+      font-family: $heading-family;
+
+      color: $primary-500;
+    }
+
+    a {
+      font-weight: 600;
+      color: $primary-500;
+    }
+  }
+  .sun {
+    width: rem(300);
+    height: rem(300);
+    color: $primary-50;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import ErrorChip from '$components/error-chip.svelte';
+  import LoadingScreen from '$components/loading-screen.svelte';
+  import MainFooter from '$components/main-footer.svelte';
+  import MainNavigation from '$components/main-navigation.svelte';
+  import { hasError, loading } from '../store/job-state';
+  import '../styles/main.scss';
+</script>
+
+{#if $hasError}
+  <ErrorChip />
+{/if}
+{#if $loading}
+  <LoadingScreen />
+{/if}
+<MainNavigation />
+<slot />
+<MainFooter />
